43c3405bd553ec90b4174aa90e9b14443e67cf1c
steveamedi <amsteve46@gmail.com>
2021-06-11 21:31:30 +0300

    validation with formik

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { StyledContainer } from "./components/Styles";
+import "./App.css";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Signup from "./pages/Signup";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <StyledContainer>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route path="/dashboard" component={Dashboard} />
-        </Switch>
-      </StyledContainer>
-    </Router>
-  );
+    <div>
+      <Home />
+    </div>
+  )
 }
 
-export default App;
+export default App
+
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,114 @@
-import React from "react";
-import {
-  StyledTitle,
-  StyledSubTitle,
-  StyledButton,
-  StyledButtonGroup,
-} from "../components/Styles";
+import { useFormik } from "formik";
 
 const Home = () => {
-  return (
-    <div>
-      <StyledTitle>
-        <h1>This is the Home page</h1>
-      </StyledTitle>
-      <StyledSubTitle size={100} color="blue">
-        <h2>This is the sub title</h2>
-      </StyledSubTitle>
-      <StyledButtonGroup>
-        <StyledButton to="/login">Log in</StyledButton>
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validate: (values) => {
+     const errors = {};
+     if(!values.firstName) {
+       errors.firstName="Required";
+     } else if(values.firstName.length > 15) {
+       errors.firstName="Must be more than 15 characters";
+     } 
 
-        <StyledButton to="/login">Sign up</StyledButton>
-      </StyledButtonGroup>
-    </div>
+     if(!values.lastName) {
+       errors.lastName = "Required";
+     } else if(values.lastName.length > 15) {
+       errors.lastName="Must be more than 15 characters";
+     }
+     
+     if(!values.email) {
+       errors.email = "Required";
+     } else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+       errors.email="Invalid email address";
+     }
+     return errors;
+    },
+    onSubmit: (values) => console.log(values),
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <h4>Sign up</h4>
+      <div className="form-group">
+        <label>First Name</label>
+        <input
+          type="text"
+          placeholder="e.g. john"
+          id="firstName"
+          name="firstName"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.firstName && formik.errors.firstName ? (<div>{formik.errors.firstName}</div>) : null}
+      </div>
+      <div className="form-group">
+        <label>Last Name</label>
+        <input
+          type="text"
+          placeholder="e.g. doe"
+          id="lastName"
+          name="lastName"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.lastName && formik.errors.lastName ? (<div>{formik.errors.lastName}</div>): null}
+      </div>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="e.g. johndoe@gmail.com"
+          id="email"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+      </div>
+      <div className="form-group">
+        <label>Date of Birth</label>
+        <select>
+          <option>January</option>
+          <option>February</option>
+          <option>March</option>
+          <option>April</option>
+          <option>May</option>
+          <option>June</option>
+        </select>
+        <select>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+        </select>
+        <select>
+          <option>1996</option>
+          <option>1995</option>
+          <option>1994</option>
+          <option>1994</option>
+        </select>
+      </div>
+
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" />
+      </div>
+      <div className="form-group">
+        <label>Confirm Password</label>
+        <input type="password" />
+      </div>
+      <button type="submit">Register</button>
+    </form>
   );
 };
 
